9afbdd0d7cabb569dcd3755415672543cd584c60
Gull-virk <aapka_email@gmail.com>
2026-07-23 12:10:33 +0500

    Initial commit for MCB Live project with Golden Package and 24h report

---
+++ script.js
@@ -0,0 +1,13 @@
+const togglePassword = document.getElementById('togglePassword');
+const passwordInput = document.getElementById('password');
+
+togglePassword.addEventListener('click', function () {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+    
+    if (type === 'text') {
+        this.src = 'eye.svg';
+    } else {
+        this.src = 'eye-off.svg';
+    }
+});
